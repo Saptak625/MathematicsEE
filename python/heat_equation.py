a2ae298f80192cd37542dbc8561c134aa9d02c52
SaptakDnth <66028584+Saptak625@users.noreply.github.com>
2023-07-11 12:29:18 -0400

    Add Properties of Fourier Transform Section

--- python/heat_equation.py
+++ python/heat_equation.py
@@ -1,34 +1,59 @@
 # Implementation of the heat equation in 1D
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from mpl_toolkits.mplot3d import axes3d
 
+a = 1    # Thermal diffusivity constant
+L = 100  # Length of domain
+N = 1000 # Number of discretization points
+dx = L/N
+x = np.arange(-L/2,L/2,dx) # Define x domain
 
-def heat_equation(x, t, u0, D, L):
-    """
-    Solves the heat equation in 1D
-    :param x: 1D array of x values
-    :param t: 1D array of t values
-    :param u0: Initial condition
-    :param D: Diffusion coefficient
-    :param L: Length of the domain
-    :return: 2D array of u(x,t)
-    """
-    # Create a 2D array to store the solution
-    u = np.zeros((len(x), len(t)))
-    # Compute the solution including the boundary conditions
-    for i in range(len(x)):
-        for j in range(len(t)):
-            u[i, j] = u0 * np.exp(-D * (np.pi / L) ** 2 * t[j]) * np.sin(np.pi * x[i] / L)
-    return u
+# Define discrete wavenumbers
+kappa = 2*np.pi*np.fft.fftfreq(N, d=dx)
 
-y = heat_equation(np.linspace(0, 1, 100), np.linspace(0, 1, 100), 1, 1, 1)
+# Initial condition
+u0 = np.zeros_like(x)
+u0[int((L/2 - L/10)/dx):int((L/2 + L/10)/dx)] = 1
+u0hat = np.fft.fft(u0)
 
-# Plot the solution using a waterfall plot
+# SciPy's odeint function doesn't play well with complex numbers, so we recast 
+# the state u0hat from an N-element complex vector to a 2N-element real vector
+u0hat_ri = np.concatenate((u0hat.real,u0hat.imag))
+
+# Simulate in Fourier frequency domain
+dt = 0.1
+t = np.arange(0,10,dt)
+
+def rhsHeat(uhat_ri,t,kappa,a):
+    uhat = uhat_ri[:N] + (1j) * uhat_ri[N:]
+    d_uhat = -a**2 * (np.power(kappa,2)) * uhat
+    d_uhat_ri = np.concatenate((d_uhat.real,d_uhat.imag)).astype('float64')
+    return d_uhat_ri
+
+uhat_ri = odeint(rhsHeat, u0hat_ri, t, args=(kappa,a))
+
+uhat = uhat_ri[:,:N] + (1j) * uhat_ri[:,N:]
+
+u = np.zeros_like(uhat)
+
+for k in range(len(t)):
+    u[k,:] = np.fft.ifft(uhat[k,:])
+
+u = u.real    
+
+# Waterfall plot
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-X, Y = np.meshgrid(np.linspace(0, 1, 100), np.linspace(0, 1, 100))
-ax.plot_wireframe(X, Y, y, rstride=10, cstride=10)
-ax.set_xlabel('x')
-ax.set_ylabel('t')
-ax.set_zlabel('u(x,t)')
+
+u_plot = u[0:-1:10,:]
+for j in range(u_plot.shape[0]):
+    ys = j*np.ones(u_plot.shape[1])
+    ax.plot(x,ys,u_plot[j,:])
+    
+# Image plot
+plt.figure()
+plt.imshow(np.flipud(u), aspect=8)
+plt.axis('off')
 plt.show()
